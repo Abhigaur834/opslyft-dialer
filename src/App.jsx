10dec8669fbece89962bab8f59738e89597ec853
OcubeServices <73984977+Abhigaur834@users.noreply.github.com>
2026-03-30 08:20:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginScreen from "./components/dialer/LoginScreen";
-import SupervisorDashboard from "./components/dialer/SupervisorDashboard";
-import AgentDesktop from "./components/dialer/AgentDesktop";
+import DialerApp from "./components/dialer/DialerApp";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginScreen />} />
-        <Route path="/supervisor" element={<SupervisorDashboard />} />
-        <Route path="/agent" element={<AgentDesktop />} />
-      </Routes>
-    </BrowserRouter>
-  );
+export default function App() {
+  return <DialerApp />;
 }
-
-export default App;

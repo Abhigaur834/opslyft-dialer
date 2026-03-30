c7b00461282c846605e62f1bb340eb755f34ab33
OcubeServices <73984977+Abhigaur834@users.noreply.github.com>
2026-03-30 08:45:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCw9Sy6vFrL1JTY9-HWQrTJsTZMhUM2Aig",
-  authDomain: "opslyft-dialer.firebaseapp.com",
-  databaseURL: "https://opslyft-dialer-default-rtdb.firebaseio.com",
-  projectId: "opslyft-dialer",
-  storageBucket: "opslyft-dialer.firebasestorage.app",
+// Exported so UserManagement.jsx can initialise a secondary app
+// to create new users without logging out the supervisor
+export const firebaseConfig = {
+  apiKey:            "AIzaSyCw9Sy6vFrL1JTY9-HWQrTJsTZMhUM2Aig",
+  authDomain:        "opslyft-dialer.firebaseapp.com",
+  databaseURL:       "https://opslyft-dialer-default-rtdb.firebaseio.com",
+  projectId:         "opslyft-dialer",
+  storageBucket:     "opslyft-dialer.firebasestorage.app",
   messagingSenderId: "571952219763",
-  appId: "1:571952219763:web:a4fd769daa1aa40f29cc06"
+  appId:             "1:571952219763:web:a4fd769daa1aa40f29cc06",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db   = getFirestore(app);
